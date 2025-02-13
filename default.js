2e4b0695ffbaa5589473361643f30662967106fc
chandlerolivia <chartenb@uoregon.edu>
2025-02-12 16:10:38 -0800

    Add files via upload

---
+++ default.js
@@ -0,0 +1,20 @@
+/*
+    Valentines default webpage
+*/
+
+/*
+const body = document.body;
+body.style.backgroundImage = "url('default.png')";
+body.style.backgroundSize = "cover";
+
+
+let bgID = document.querySelector("#bg");
+bgID.style.backgroundImage = "url('default.png')"
+*/
+
+window.onload = function() {
+    const body = document.body;
+    body.style.backgroundImage = "url('default.png')";
+    body.style.backgroundSize = "cover";
+    body.style.backgroundColor = "lightblue";
+}
